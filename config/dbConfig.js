17f57124f8833c356086ebd051409a694a0d753d
Adnan4141 <adnanh4141@gmail.com>
2025-03-21 02:51:39 +0600

    cors setup22

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,9 @@ const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/chat-application"
 
 const connectDB = async()=>{
 try {
-   const conn = await mongoose.connect(dbUrl)
+   const conn = await mongoose.connect(dbUrl,{
+      serverSelectionTimeoutMS: 5000,
+   })
    console.log(`Mongodb connected at: ${conn.connection.host}`)
 } catch (error) {
    console.log(`Mongodb Connection Error: ${error.message}`)
